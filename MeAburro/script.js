3e7eb88740de4c97720183a34ba06c2edbb6f009
AlbertoRodriguezPenalva <151755187+AlbertoRodriguezPenalva@users.noreply.github.com>
2024-04-11 15:57:46 +0200

    cards mas grandes

--- MeAburro/script.js
+++ MeAburro/script.js
@@ -1,14 +1,25 @@
+var timeout; // Variable para almacenar el temporizador
+
 window.addEventListener('scroll', function() {
     var navItems = document.querySelectorAll('.nav > li > a');
     navItems.forEach(function(item) {
-      var bounding = item.getBoundingClientRect();
-      if (bounding.top < window.innerHeight / 2 && bounding.bottom > window.innerHeight / 2) {
-        item.style.opacity = 1; // Si el elemento está en la mitad de la ventana, establece opacidad a 1
-      } else {
-        item.style.opacity = 0.5; // Si el elemento no está en la mitad de la ventana, establece opacidad a 0.5
-      }
+        var bounding = item.getBoundingClientRect();
+        if (bounding.top < window.innerHeight / 2 && bounding.bottom > window.innerHeight / 2) {
+            item.style.opacity = 1; // Si el elemento está en la mitad de la ventana, establece opacidad a 1
+        } else {
+            item.style.opacity = 0.5; // Si el elemento no está en la mitad de la ventana, establece opacidad a 0.5
+        }
     });
-  });
+
+    // Restablecer el color después de 5 segundos de inactividad
+    clearTimeout(timeout); // Borrar el temporizador existente
+    timeout = setTimeout(function() {
+        navItems.forEach(function(item) {
+            item.style.opacity = 1; // Restablecer el color a su estado original
+        });
+    }, 5000); // 5000 milisegundos (5 segundos)
+});
+
 
 function avisa() {
     location.reload();
